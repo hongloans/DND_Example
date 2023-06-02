5a71bf8c3e23db1245b9d7b0674c627be38ebb88
홍지운 <loans93@gmail.com>
2023-06-03 00:12:52 +0900

    테마 및 분리 컴포넌트 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import router from "./pages/router";
 import { RouterProvider } from "react-router-dom";
+import MainTheme from "./mainTheme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <MainTheme>
+      <RouterProvider router={router} />
+    </MainTheme>
   </React.StrictMode>
 );
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import TextButton from "../../components/common/TextButton";
+
 function Main() {
+  const navigate = useNavigate();
+  const handleListClick = () => {
+    navigate("/list");
+  };
+  const handleGridClick = () => {
+    navigate("/grid");
+  };
   return (
-    <div>
-      <button />
+    <div className="w-full container flex flex-col items-center justify-center gap-10">
+      <TextButton title="DND LIST" onClick={handleListClick} />
+      <TextButton title="DND GRID" onClick={handleGridClick} />
     </div>
   );
 }
